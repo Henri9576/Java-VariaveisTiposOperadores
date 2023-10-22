e6beb734ac379deff1f01b27460f0c5cd1cd62a6
Henrique9576 <henrique_costa02@outlook.com>
2023-10-22 17:29:49 -0300

    Initial commit

---
+++ app/src/main/java/Aula4FVariaveisTiposOperadores/App.java
@@ -0,0 +1,23 @@
+package Aula4FVariaveisTiposOperadores;
+
+public class App {
+
+    public static void main(String[] args) {
+        
+        int idade = 0; //armazena um numero inteiro até o integer
+        float cotacaoDolar = 5.0f; //armazena numeros reais
+        double cotacaoEuro = 6.0d;//armazena numeros decimais
+        char genero = 'M'; //armazena uma letra
+        byte pontos = 0; //armazena um numero inteiro até 128
+        boolean estaCadastrado = false; //armazena sim ou não
+        
+        String nome = "Esse é um texto de uma variável String";
+        /*
+        + adição
+        - subtração
+        / divisão
+        * multiplicação
+        % porcentagem
+                */
+    }
+}
